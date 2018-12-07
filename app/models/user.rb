b888082a5b556afc342b97adeeacc60355aab92f
Mekarosi Assa <mekarosi_assa@yahoo.com>
2018-12-07 14:29:08 +0000

    Added profile and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
+  has_one :profile
   attr_accessor :stripe_card_token
   # If Pro users passes validation (email, password etc)
   # then call Stripe and tell Stripe to set up a subscription
