03650eb248419ec2cb9ee423f6f24408634fe935
Mekarosi Assa <mekarosi_assa@yahoo.com>
2018-12-16 15:51:58 +0000

    Added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,25 @@
 class ProfilesController < ApplicationController
   # GET to /user/:user_id/profile/new
     def new
-    # Render blanck profile details form
-    @profile = Profile.new
+      # Render blanck profile details form
+      @profile = Profile.new
     end
-end    
+    
+    # POST to /users/:user_id/profile
+    def create
+    # Ensure that we have the user who is filling out form
+      @user = User.find( params[:user_id] )
+    # Create profile linked to this specific user
+      @profile = @user.build_profile( profile_param )
+        if @profile.save
+          flash[:success] = "profile updated!"
+          redirect_to root_path
+        else
+          render action: :new
+        end    
+     end   
+     
+  Private
+    def profile_params
+      params.require (:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description )
+    end  
