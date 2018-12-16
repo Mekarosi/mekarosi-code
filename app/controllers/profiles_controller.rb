198a2b23020894435290e24824116973ca7056da
Mekarosi Assa <mekarosi_assa@yahoo.com>
2018-12-16 15:29:26 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,6 @@ class ProfilesController < ApplicationController
   # GET to /user/:user_id/profile/new
     def new
     # Render blanck profile details form
+    @profile = Profile.new
     end
 end    
